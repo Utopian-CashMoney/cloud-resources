f164350762af92138c39d8ff0cda3b34d3a4eb15
NickMead13 <Nick.Mead@Smoothstack.com>
2021-07-26 11:02:51 -0500

    Update main.tf

--- Terraform/modules/microservices/main.tf
+++ Terraform/modules/microservices/main.tf
@@ -79,10 +79,10 @@ resource "aws_ecs_task_definition" "user_ms" {
 resource "aws_lb" "alb" {
   name               = "cashmoney-alb"
   internal           = true
-  load_balancer_type = "network"
+  load_balancer_type = "application"
   subnets            = var.private_subnet_ids
 
-  enable_deletion_protection = true
+  enable_deletion_protection = false
 }
 
 resource "aws_lb_target_group" "user_ms" {
@@ -97,8 +97,6 @@ resource "aws_lb_listener" "listener" {
   load_balancer_arn = aws_lb.alb.arn
   port              = "8000"
   protocol          = "HTTP"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
 
   default_action {
     type             = "forward"
